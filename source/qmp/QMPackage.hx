32d04a11cec5a6cb7f85037cbedd38666cbd3766
Aura <jankonstanta@gmail.com>
2023-08-09 15:35:53 +0800

    Add untracked files

---
+++ source/qmp/QMPackage.hx
@@ -0,0 +1,29 @@
+package qmp;
+
+enum QMPackageVersion
+{
+	VERSION_FIRST;
+}
+
+enum QMPackageType
+{
+	TYPE_SONG;
+}
+
+typedef QMPackageInfo =
+{
+	var Version:QMPackageVersion;
+	var Type:QMPackageType;
+}
+
+typedef QMPackageEntry =
+{
+	var Name:String;
+	var Data:String;
+}
+
+typedef QMPackage =
+{
+	var Info:QMPackageInfo;
+	var Entries:Array<QMPackageEntry>;
+}
